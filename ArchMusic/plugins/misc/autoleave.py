500085424ecffd30ba565fb61938656da1b3a519
Mehmetbeydiyceksiniz63 <149189769+Mehmetbeydiyceksiniz63@users.noreply.github.com>
2025-03-05 18:28:07 +0300

    Update autoleave.py

--- ArchMusic/plugins/misc/autoleave.py
+++ ArchMusic/plugins/misc/autoleave.py
@@ -21,13 +21,16 @@ from ArchMusic.utils.database import (get_client, is_active_chat,
 
 async def auto_leave():
     if config.AUTO_LEAVING_ASSISTANT == str(True):
-        while not await asyncio.sleep(config.AUTO_LEAVE_ASSISTANT_TIME):
-            from AlexaMusic.core.userbot import assistants
+        while not await asyncio.sleep(
+            config.AUTO_LEAVE_ASSISTANT_TIME
+        ):
+            from ArchMusic.core.userbot import assistants
 
             for num in assistants:
                 client = await get_client(num)
+                left = 0
                 try:
-                    async for i in client.get_dialogs():
+                    async for i in client.iter_dialogs():
                         chat_type = i.chat.type
                         if chat_type in [
                             ChatType.SUPERGROUP,
@@ -37,11 +40,18 @@ async def auto_leave():
                             chat_id = i.chat.id
                             if (
                                 chat_id != config.LOG_GROUP_ID
-                                and chat_id != -1001686672798
+                                and chat_id != -1001687657146
+                                and chat_id != -1001687657146
+                                and chat_id != -1001687657146
                             ):
+                                if left == 20:
+                                    continue
                                 if not await is_active_chat(chat_id):
                                     try:
-                                        await client.leave_chat(chat_id)
+                                        await client.leave_chat(
+                                            chat_id
+                                        )
+                                        left += 1
                                     except:
                                         continue
                 except:
@@ -52,32 +62,29 @@ asyncio.create_task(auto_leave())
 
 
 async def auto_end():
-    while True:
-        await asyncio.sleep(30)
-        for chat_id, timer in list(autoend.items()):
+    while not await asyncio.sleep(5):
+        if not await is_autoend():
+            continue
+        for chat_id in autoend:
+            timer = autoend.get(chat_id)
+            if not timer:
+                continue
             if datetime.now() > timer:
                 if not await is_active_chat(chat_id):
-                    del autoend[chat_id]
+                    autoend[chat_id] = {}
                     continue
-                userbot = await get_assistant(chat_id)
-                members = []
-                async for member in userbot.get_call_members(chat_id):
-                    if member is None:
-                        continue
-                    members.append(member)
-                if len(members) <= 1:
-                    try:
-                        await Alexa.stop_stream(chat_id)
-                    except Exception:
-                        pass
-                    try:
-                        await app.send_message(
-                            chat_id,
-                            "ʙᴏᴛ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴄʟᴇᴀʀᴇᴅ ᴛʜᴇ ǫᴜᴇᴜᴇ ᴀɴᴅ ʟᴇғᴛ ᴠɪᴅᴇᴏᴄʜᴀᴛ ʙᴇᴄᴀᴜsᴇ ɴᴏ ᴏɴᴇ ᴡᴀs ʟɪsᴛᴇɴɪɴɢ sᴏɴɢs ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-                        )
-                    except Exception:
-                        pass
-                del autoend[chat_id]
+                autoend[chat_id] = {}
+                try:
+                    await ArchMusic.stop_stream(chat_id)
+                except:
+                    continue
+                try:
+                    await app.send_message(
+                        chat_id,
+                        "**Sesli Sohbette Kimse Olmadığı İçin, Bot Parçayı Sonlandırdı** 🙂‍↕️",
+                    )
+                except:
+                    continue
 
 
 asyncio.create_task(auto_end())
